675e01fe6fdcfacd01dadbe2c755a85375c1c9a8
jamarbo <jamarbo@hotmail.com>
2023-11-09 18:48:42 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende React
+          Aprendamos React
         </a>
       </header>
     </div>
